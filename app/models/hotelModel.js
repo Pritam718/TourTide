6b8ea2c9faeb92a49cdb8378a8580833725c3fef
Pritam Adhikari <pritam762.hitcsecs2020@gmail.com>
2025-06-10 12:57:46 +0530

    hotel model add

---
+++ app/models/hotelModel.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const hotelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    bedRoom: {
+      type: Number,
+      required: true,
+    },
+    hallRoom: {
+      type: Number,
+      required: true,
+    },
+    kitchen: {
+      type: Number,
+      required: true,
+    },
+    bathRoom: {
+      type: Number,
+      required: true,
+    },
+    occupancy: {
+      minPerson: {
+        type: Number,
+        required: true,
+      },
+      maxPerson: {
+        type: Number,
+        required: true,
+      },
+      extraAdult: {
+        type: Number,
+      },
+      extraChild: {
+        type: Number,
+      },
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    childPrice: {
+      type: Number,
+    },
+    accommodation: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Hotel = mongoose.model("Hotel", hotelSchema);
+module.exports = Hotel;

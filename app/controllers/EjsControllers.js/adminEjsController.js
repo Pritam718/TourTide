109d1f2e37e52729f7ac62311bb755f3f4121c96
Pritam Adhikari <pritam762.hitcsecs2020@gmail.com>
2025-07-23 10:43:58 +0530

    update food and image

--- app/controllers/EjsControllers.js/adminEjsController.js
+++ app/controllers/EjsControllers.js/adminEjsController.js
@@ -120,7 +120,7 @@ class AdminEjsController {
         },
       ]);
       console.log(data);
-      // res.json(data);
+      // return res.json(data);
       res.render("foodTable", { foods: data, user: req.user || null });
     } catch (error) {
       console.log(error);
